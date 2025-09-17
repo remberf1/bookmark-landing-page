b4c7841417f775644829cf2326723a1dfbf4e5e9
remberf1 <68003095+remberf1@users.noreply.github.com>
2025-09-17 03:11:45 +0100

    mobile view mainly done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const tabs = document.querySelectorAll('.tab-table button');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+  });
+});
